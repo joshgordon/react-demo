0e16018be5eb0425d13e6debaeb9df09a737c565
Josh Gordon <github@joshgordon.net>
2016-04-30 09:41:16 -0400

    Adding styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { CommentBox } from './components/CommentBox';
 
+require('./style.css');
+
 ReactDOM.render (
   <CommentBox/>,
   document.getElementById('react-mount')
